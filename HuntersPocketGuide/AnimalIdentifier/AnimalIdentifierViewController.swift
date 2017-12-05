5baec0597feed87b83b2e5fc3f4bc384fb4d63ed
Tom Dolan <tomdolan@Toms-MacBook-Pro.local>
2017-12-05 00:58:06 -0800

    waterfall images implemented with bugs

--- HuntersPocketGuide/AnimalIdentifier/AnimalIdentifierViewController.swift
+++ HuntersPocketGuide/AnimalIdentifier/AnimalIdentifierViewController.swift
@@ -11,13 +11,33 @@ import CHTCollectionViewWaterfallLayout
 
 class AnimalIdentifierViewController: UIViewController, CHTCollectionViewDelegateWaterfallLayout, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    let reuseIdentifier = "test"
+    
     public init() {
         super.init(nibName: nil, bundle: nil)
-        view.backgroundColor = UIColor.red
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    func setupView() {
+        view.backgroundColor = UIColor.red
+        let layout = CHTCollectionViewWaterfallLayout.init()
+        let collectionView = UICollectionView.init(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        self.view.addSubview(collectionView)
+        collectionView.autoPinEdgesToSuperviewMargins()
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
     
     // MARK: UICollectionViewDelegate
@@ -26,18 +46,52 @@ class AnimalIdentifierViewController: UIViewController, CHTCollectionViewDelegat
     }
     
     // MARK: UICollectionViewDataSource
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0;
+        return 100
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as UICollectionViewCell
+        
+//        cell.translatesAutoresizingMaskIntoConstraints = false
+
+        var image: UIImage? = nil
+        if indexPath.row == 0 {
+            image = UIImage(named: "AmericanRedSquirrel.png")
+        } else if indexPath.row % 7 == 0 {
+            image = UIImage(named: "DouglasSquirrel.png")
+        } else if indexPath.row % 5 == 0 {
+            image = UIImage(named: "EasternFoxSquirrel.png")
+        } else if indexPath.row % 3 == 0 {
+            image = UIImage(named: "EasternGraySquirrel.png")
+        } else if indexPath.row % 2 == 0 {
+            image = UIImage(named: "NorthernFlyingSquirrel.png")
+        } else {
+            image = UIImage(named: "WesternGraySquirrel.png")
+        }
+        
+        let imageView = UIImageView(image: image!)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        cell.contentView.addSubview(imageView)
+        imageView.autoPinEdgesToSuperviewMargins()
+        return cell
     }
     
     // MARK: CHTCollectionViewDelegateWaterfallLayout
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
-        return CGSize.zero
+        let unwrapedCell = collectionView.cellForItem(at: indexPath)
+        
+        if let cell = unwrapedCell {
+            let size = CGSize.init(width: collectionView.frame.size.width, height:cell.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height )
+            return size
+        }
+        
+        return CGSize.init(width: collectionView.frame.size.width, height: 100)
     }
-    
-    
 }
